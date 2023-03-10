583024dfa19ffb4f2653d5229ff2e86c2b6af031
Walter Santos Filho <wcsantosfilho@gmail.com>
2023-03-10 06:50:31 -0300

    alterações de logo, favicon e html

--- baitaca/src/components/workingArea.js
+++ baitaca/src/components/workingArea.js
@@ -1,13 +1,15 @@
 import React, { useState} from 'react';
-import { Box } from 'grommet';
+import { Box, Spinner, Text } from 'grommet';
 import axios from 'axios';
 import FormSearch from './formSearch.js'
 import DataArea from './dataArea.js';
 
 const workingArea = () => {
   const [activeSearch, setactiveSearch] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleFormSearchChange = (value) => {
+    setLoading(true);
     let urlBase = process.env.REACT_APP_API_URL;
     let uri = urlBase + value.numberOfLetters + '/' + value.initial + '/numberOfLettersAndInitial'
     axios.get(uri)
@@ -27,11 +29,15 @@ const workingArea = () => {
             setactiveSearch([{word: "Server is out of service"}]);
         }
     })
+    .finally(() => {
+      setLoading(false)
+    })
   }
 
   const handleFormReset = () => {
     setactiveSearch([{word: "Search Reseted"}]);
   }
+
 
   return (
     <div className="workingArea">
@@ -40,6 +46,21 @@ const workingArea = () => {
           onFormSearchChange={handleFormSearchChange}
           onFormSearchReset={handleFormReset}
         />
+        {loading &&
+          <Box align="center" direction="row" gap="small" pad="small">
+            <Spinner
+              border={[
+                {
+                  side: 'all',
+                  color: 'brand',
+                  size: 'medium',
+                  style: 'dotted',
+                },
+              ]}
+            />
+            <Text>Loading...</Text>
+          </Box>
+        }
         <DataArea dataArray={activeSearch} />
       </Box>
     </div>
